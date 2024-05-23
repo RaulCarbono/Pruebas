d2058dd739333711628746b650150696832e7aab
Raul Carbono <carbonoraul2@gmail.com>
2024-05-23 16:56:05 -0500

    bold

---
+++ bold/src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from '../header/header.module.css'
+const Header = () => {
+  return (
+    <div className={styles.container}>Hola</div>
+  )
+}
+
+export default Header

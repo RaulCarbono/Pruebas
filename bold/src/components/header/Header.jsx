57ae6f631ada9efbc85a74280454600540d302ea
Raul Carbono <carbonoraul2@gmail.com>
2024-05-24 16:58:37 -0500

    changes

--- bold/src/components/header/Header.jsx
+++ bold/src/components/header/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import styles from '../header/header.module.css'
+import styles from '../header/header.module.css';
+import logo from '../../assets/logo.png';
 const Header = () => {
   return (
-    <div className={styles.container}>Hola</div>
+    <header >
+    <div className={styles.container}>
+      <ul> <a href=""><img src=""alt="" /></a><a href="">adios</a><a href="">sobre</a><a href="">hola</a><a href="">todo bien</a></ul>
+      
+     
+    </div>
+    </header>
   )
 }
 
